110bdc8b910f097b875b73f13f41924634beee6a
christiamrojas <crojas@diacsa.com>
2021-04-28 16:14:37 -0500

    Erase

--- Telemetry_Esp/include/config.h
+++
@@ -1,7 +0,0 @@
-#ifndef CONFIG_H
-#define CONFIG_H
-
-//#define TEL_DEBUG true
-#define OTA_ENABLED  true
-
-#endif
--- Telemetry_Esp/include/telnet.h
+++
@@ -1,8 +0,0 @@
-#ifndef TELNET_H
-#define TELNET_H
-
-#define DNS_HOST_NAME "ecm3fs_host"
-
-void Tel_debug_init(void);
-
-#endif
